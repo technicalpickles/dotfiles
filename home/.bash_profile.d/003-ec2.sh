9042b4bc01447dd77777d5a5d6bea7e5cdd80db9
Josh Nichols <josh@technicalpickles.com>
2012-12-06 18:24:15 -0500

    Shhh, it's okay if those aren't there

--- home/.bash_profile.d/003-ec2.sh
+++ home/.bash_profile.d/003-ec2.sh
@@ -1,4 +1,4 @@
 export EC2_HOME=~/.ec2
 export PATH=${PATH}:${EC2_HOME}/bin
-export EC2_PRIVATE_KEY=$(ls $EC2_HOME/pk-*.pem)
-export EC2_CERT=$(ls $EC2_HOME/cert-*.pem)
+export EC2_PRIVATE_KEY=$(ls $EC2_HOME/pk-*.pem 2>/dev/null)
+export EC2_CERT=$(ls $EC2_HOME/cert-*.pem 2>/dev/null)
