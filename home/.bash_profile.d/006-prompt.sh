aff1ee13faeeded315fb8ef0ae3d618a64724260
Josh Nichols <josh@technicalpickles.com>
2012-12-06 18:27:29 -0500

    Make sure rvm prompt && __git_ps1 are available before using them in PS1

--- home/.bash_profile.d/006-prompt.sh
+++ home/.bash_profile.d/006-prompt.sh
@@ -1,7 +1,13 @@
 # needs 001-colors.sh
 
 PS1_LINE1="${BRIGHT_GREEN}———————————————— macbook-pro \t ————————————————"
-PS1_LINE2="${BRIGHT_RED}\$(~/.rvm/bin/rvm-prompt)${RESET} in ${BRIGHT_YELLOW}\w${BRIGHT_CYAN} \$(__git_ps1 '(%s) ')"
+
+if [[ -x ~/.rvm/bin/rvm-prompt && -x __git_ps1 ]]; then
+	PS1_LINE2="${BRIGHT_RED}\$(~/.rvm/bin/rvm-prompt)${RESET} in ${BRIGHT_YELLOW}\w${BRIGHT_CYAN} \$(__git_ps1 '(%s) ')"
+else
+	PS1_LINE2="${BRIGHT_YELLOW}\w"
+fi
+
 PS1_LINE3="${BRIGHT_BLUE}\$${RESET} "
 
 export  PS1="\n${PS1_LINE1}\n${PS1_LINE2}\n${PS1_LINE3}"
