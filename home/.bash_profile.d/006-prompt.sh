270bcd2022c0da0dec2e05e905b1b8aa8c7e9dfa
Josh Nichols <technicalpickles@github.com>
2013-07-10 12:29:55 -0400

    Bring back __git_ps1 and add ruby version things

--- home/.bash_profile.d/006-prompt.sh
+++ home/.bash_profile.d/006-prompt.sh
@@ -2,12 +2,20 @@
 
 PS1_LINE1="${BRIGHT_GREEN}———————————————— \h \t ————————————————"
 
-if [[ -x ~/.rvm/bin/rvm-prompt && -x __git_ps1 ]]; then
-	PS1_LINE2="${BRIGHT_RED}\$(~/.rvm/bin/rvm-prompt)${RESET} in ${BRIGHT_YELLOW}\w${BRIGHT_CYAN} \$(__git_ps1 '(%s) ')"
-else
-	PS1_LINE2="${BRIGHT_YELLOW}\w"
+if type __git_ps1 | grep -q "is a function"; then
+	vcs_prompt="${BRIGHT_CYAN} \$(__git_ps1 '(%s) ')"
 fi
 
+
+if [[ -x ~/.rvm/bin/rvm-prompt ]]; then
+	ruby_prompt="${BRIGHT_RED}\$(~/.rvm/bin/rvm-prompt)${RESET} "
+fi
+
+if type rbenv | grep -q "is a function" || which -s rbenv; then
+	ruby_prompt="${BRIGHT_RED}\$(rbenv version | cut -f1 -d' ')${RESET} "
+fi
+
+PS1_LINE2="${ruby_prompt}${BRIGHT_YELLOW}\w${vcs_prompt}"
 PS1_LINE3="${BRIGHT_BLUE}\$${RESET} "
 
-export  PS1="\n${PS1_LINE1}\n${PS1_LINE2}\n${PS1_LINE3}"
+export PS1="\n${PS1_LINE1}\n${PS1_LINE2}\n${PS1_LINE3}"
