6811bd4d643759d5e91db7610d21c4c27190b0dd
Josh Nichols <technicalpickles@github.com>
2013-07-10 12:11:36 -0400

    Don't hardcode host

--- home/.bash_profile.d/006-prompt.sh
+++ home/.bash_profile.d/006-prompt.sh
@@ -1,6 +1,6 @@
 # needs 001-colors.sh
 
-PS1_LINE1="${BRIGHT_GREEN}———————————————— macbook-pro \t ————————————————"
+PS1_LINE1="${BRIGHT_GREEN}———————————————— \h \t ————————————————"
 
 if [[ -x ~/.rvm/bin/rvm-prompt && -x __git_ps1 ]]; then
 	PS1_LINE2="${BRIGHT_RED}\$(~/.rvm/bin/rvm-prompt)${RESET} in ${BRIGHT_YELLOW}\w${BRIGHT_CYAN} \$(__git_ps1 '(%s) ')"
