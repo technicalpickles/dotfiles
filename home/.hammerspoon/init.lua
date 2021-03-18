5bb9a53e3a4f778eb2db70a6bb9b1ab68a4dc11e
Josh Nichols <joshua.nichols@gmail.com>
2021-03-18 14:05:21 -0400

    Refactor .gitconfig - add static snippets in .gitconfig.d - generate .gitconfig.local in install.sh

--- home/.hammerspoon/init.lua
+++ home/.hammerspoon/init.lua
@@ -1,5 +1,15 @@
 require("hs.ipc")
+require("hs.application")
+require("hs.screen")
+
 hs.ipc.cliInstall()
+
+
+package.path = package.path .. ';/usr/local/opt/lua@5.3/share/lua/5.3/?.lua'
+
+require("luarocks.loader")
+inspect = require('inspect')
+
 
 hs.loadSpoon('ControlEscape'):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 
@@ -24,3 +34,25 @@ screenWatcher = hs.screen.watcher.newWithActiveScreen(function(screenLayoutChang
 	end
 end)
 screenWatcher:start()
+
+function relocateMacbreakzActivityWindow()
+  local app = hs.application.get("net.publicspace.mb5")
+  app:activate()
+  
+
+  local activityWindow = app:focusedWindow()
+  local screen = hs.screen.primaryScreen()
+  local frame = activityWindow:frame()
+  local max = screen:fullFrame()
+  
+  local cornerPoint = hs.geometry.point(max.x, max.y)
+  local corner = screen:localToAbsolute()
+  
+  -- reminder: 0,0 is top left
+  frame.x = max.x
+  frame.y = max.y
+  activityWindow:setFrame(frame)
+end
+ 
+ -- relocateMacbreakzActivityWindow()
+
