c14f40f5a3e3bc09ef5d3b44a1c68c5772ed828f
Josh Nichols <josh.nichols@gusto.com>
2021-09-29 10:01:44 -0400

    fix first install of fish

--- fish.sh
+++ fish.sh
@@ -4,19 +4,19 @@ if ! test -d "${HOME}/.local/share/omf/"; then
   curl -L https://get.oh-my.fish | fish
 fi
 
+rm -f ~/.config/omf/bundle
 
 theme=bobthefish
-echo theme $theme > ~/.config/omf/bundle
-if ! test -d "${HOME}/.local/share/omf/themes/bobthefish"; then
-  omf theme $theme
+if ! test -d "${HOME}/.local/share/omf/themes/${theme}"; then
+  echo theme $theme >> ~/.config/omf/bundle
 fi
 
 rm ~/.config/omf/bundle
-echo package foreign-env > ~/.config/omf/bundle
+echo package foreign-env >> ~/.config/omf/bundle
 
 for package in direnv nodenv pyenv rbenv thefuck; do
   if which "$package" >/dev/null; then
-    echo package "$package" > ~/.config/omf/bundle
+    echo package "$package" >> ~/.config/omf/bundle
   fi
 done
 
