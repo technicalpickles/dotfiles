d2a91991d63102d6292bee8db306840d70c0b2fc
Josh Nichols <joshua.nichols@gmail.com>
2021-11-29 08:47:21 -0500

    Add custom adr autocompletions

--- config/fish/completions/adr.fish
+++ config/fish/completions/adr.fish
@@ -27,6 +27,7 @@ complete --no-files --command adr --condition "not __fish_seen_subcommand_from (
 # TODO use __fish_adr_numbers or similar to autocomplete
 complete --no-files --command adr --condition "__fish_seen_subcommand_from new" -s l -d 'Links new ADR to a previous ADR' -a '(__fish_adr_numbers)'
 complete --no-files --command adr --condition "__fish_seen_subcommand_from new" -s s -d 'A reference (number or partial filename) of a previous decision that the new decision supercedes.' -a '(__fish_adr_numbers)'
-
+# don't autocomplete on file names for new command
+complete --no-files --command adr --condition "__fish_seen_subcommand_from new"
 complete --no-files --command adr --condition "__fish_seen_subcommand_from help" -a "(__fish_adr_commands)"
 # complete --no-files --command adr -s h -l help -d 'print help'
