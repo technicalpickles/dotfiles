e48384bfa04e1204b42f7c33d69d69a1bca1c265
Josh Nichols <josh.nichols@gusto.com>
2021-11-29 08:54:55 -0500

    switch to homebrew package for yvm fish

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -71,4 +71,9 @@ if [ -n "$HOMEBREW_PREFIX" ]
   if [ -f $HOMEBREW_PREFIX/share/chruby/auto.fish ]
     source "$HOMEBREW_PREFIX/share/chruby/auto.fish"
   end
+
+  set -x YVM_DIR $HOMEBREW_PREFIX/opt/yvm
+  if [ -r $YVM_DIR/yvm.fish ]
+    source $YVM_DIR/yvm.fish
+  end
 end
