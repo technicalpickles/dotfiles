cfe5717ad55dfba482ca7e250c4c781738c45dfe
Josh Nichols <josh.nichols@gusto.com>
2023-04-22 15:51:14 -0400

    refactor ruby item a bit. improve readability

--- config/fish/functions/_tide_item_ruby.fish
+++ config/fish/functions/_tide_item_ruby.fish
@@ -1,3 +1,15 @@
+# custom ruby item, which isn't hard-coded to chruby
 function _tide_item_ruby
-    test -e .ruby-version -o -n "$RUBY_VERSION" && _tide_print_item chruby $tide_chruby_icon' ' (ruby --version | sed -e 's/ruby //' -e 's/p.*$//')
+  # only show when there's an explicit configuration or environment variable
+  if test -e .ruby-version \
+    -o -n "$RUBY_VERSION" \
+    -o -n "$RBENV_VERSION" \
+    -o -n "$ASDF_RUBY_VERSION" \
+
+    # example output: ruby 3.1.4p223 (2023-03-30 revision 957bb7cb81) [arm64-darwin22]
+    set ruby_version (ruby --version | sed -e 's/ruby //' -e 's/p.*$//')
+    # re-use chruby so will appear the same for
+    # print item     with this name   and this icon           with this text
+    _tide_print_item chruby           $tide_chruby_icon' '    "$ruby_version"
+  end
 end
