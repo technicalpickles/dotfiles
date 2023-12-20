69263ea50762ff495d4d36f04d69240821b06022
Josh Nichols <josh.nichols@gusto.com>
2023-12-20 17:43:33 -0500

    moving to 1password for things. populate .gitconfig.local from 1password

--- gitconfig.sh
+++ gitconfig.sh
@@ -30,21 +30,26 @@ if running_macos; then
   git config --file ~/.gitconfig.local --add include.path ~/.gitconfig.d/running_macos
 
   if test -d '/Applications/1Password.app/'; then
-    echo "  → enabling 1password ssh key signing"
-    op_ensure_signed_in
-
-    git config --file ~/.gitconfig.local --add include.path ~/.gitconfig.d/1password
-
-    git config --file ~/.gitconfig.d/1password gpg.format ssh
-    git config --file ~/.gitconfig.d/1password gpg.ssh.program "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
-    git config --file ~/.gitconfig.d/1password commit.gpgsign true
-
-    signing_key=$(op item list  --tags 'ssh signing','work' --format=json | op item get - --fields 'public key')
+    signing_key=$(op item list  --tags "ssh signing","$DOTPICKLES_ROLE" --format=json | op item get - --fields 'public key')
     if [[ -n "$signing_key" ]]; then
-      git config --file ~/.gitconfig.d/1password user.signingkey "$signing_key"
+      echo "  → enabling 1password ssh key signing"
+      op_ensure_signed_in
+
+      git config --file ~/.gitconfig.local --add include.path ~/.gitconfig.d/1password
+
+      git config --file ~/.gitconfig.local gpg.format ssh
+      git config --file ~/.gitconfig.local gpg.ssh.program "/Applications/1Password.app/Contents/MacOS/op-ssh-sign"
+      git config --file ~/.gitconfig.local commit.gpgsign true
+
+      signing_key=$(op item list  --tags 'ssh signing','work' --format=json | op item get - --fields 'public key')
+      if [[ -n "$signing_key" ]]; then
+        git config --file ~/.gitconfig.local user.signingkey "$signing_key"
+      else
+        echo "uh oh, couldn't find an SSH key in 1password to use" >&2
+        exit 1
+      fi
     else
-      echo "uh oh, couldn't find an SSH key in 1password to use" >&2
-      exit 1
+      echo "  → skipping 1password ssh key signing, couldn't find singing key for ${DOTPICKLES_ROLE}" >&2
     fi
   fi
 fi
