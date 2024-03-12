35b8f5ac637d6d1a2b8c35c156dd1dbfcb95f748
Josh Nichols <josh.nichols@gusto.com>
2024-03-12 09:38:05 -0400

    document it

--- fortune.sh
+++ fortune.sh
@@ -8,8 +8,10 @@ for file in fortunes/*.yaml; do
   fortune_basename=$(basename "$file" | sed -e 's/\.yaml$//')
   echo "🔮 Scrying $fortune_basename"
   fortune_file="$fortunes_dir/$fortune_basename"
-  yq ea  'join("\n%\n")' "$file" > "$fortune_file"
+  # fortune files are \n%\n delimited
+  yq ea 'join("\n%\n")' "$file" > "$fortune_file"
 
+  # generate the .dat so fortune can access
   strfile -s "$fortune_file"
 
   paste <(echo "") <(fortune "$fortune_basename" | fold -sw 80)
