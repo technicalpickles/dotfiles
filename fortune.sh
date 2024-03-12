3bdf7c0986255583cff33cc7c4a4cd6192c634bf
Josh Nichols <josh.nichols@gusto.com>
2024-03-12 09:36:59 -0400

    cleanup output of fortune.sh

--- fortune.sh
+++ fortune.sh
@@ -1,17 +1,16 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -e
 
 fortunes_dir="$(brew --prefix fortune)/share/games/fortunes/"
 echo "Processing fortunes/*.yaml"
 
 for file in fortunes/*.yaml; do
-  echo "Found $file"
   fortune_basename=$(basename "$file" | sed -e 's/\.yaml$//')
-  echo "Generating $fortune_file it"
+  echo "🔮 Scrying $fortune_basename"
   fortune_file="$fortunes_dir/$fortune_basename"
   yq ea  'join("\n%\n")' "$file" > "$fortune_file"
 
-  strfile "$fortune_file"
-  echo "Making sure it works..."
-  fortune $fortune_basename
+  strfile -s "$fortune_file"
+
+  paste <(echo "") <(fortune "$fortune_basename" | fold -sw 80)
 done
