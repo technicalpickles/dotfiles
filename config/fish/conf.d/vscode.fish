26edf4ce810c31e1f40ec68582e53e49368be5ba
Josh Nichols <josh.nichols@gusto.com>
2024-11-26 10:49:44 -0500

    Handle cursor too

--- config/fish/conf.d/vscode.fish
+++ config/fish/conf.d/vscode.fish
@@ -1,5 +1,9 @@
-if string match -q "$TERM_PROGRAM" "vscode-insiders"
-  . (code --locate-shell-integration-path fish)
-else if string match -q "$TERM_PROGRAM" "vscode"
+if string match -q "vscode" "$TERM_PROGRAM"
+  if string match -q "*Cursor.app*" "$XDG_DATA_DIRS" && type -q cursor
+    . (cursor --locate-shell-integration-path fish)
+  else if type -q code
+    . (code --locate-shell-integration-path fish)
+  end
+else if string match -q "vscode-insiders" "$TERM_PROGRAM" && type -q code-insiders
   . (code-insiders --locate-shell-integration-path fish)
 end
