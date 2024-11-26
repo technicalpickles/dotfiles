278220320b28f0007ddd8fc7505253e0c9584abd
Josh Nichols <josh.nichols@gusto.com>
2024-11-26 10:58:03 -0500

    fix some functions

--- config/fish/functions/fish_greeting.fish
+++ config/fish/functions/fish_greeting.fish
@@ -1,5 +1,7 @@
 function fish_greeting
-  if test -d ~/workspace/fancy-motd
+  if type welcome2u >/dev/null 2>&1
+    welcome2u
+  else if test -d ~/workspace/fancy-motd
     # fancy-motd uses `declare -A`, which isn't available on the default macOS bash (3.2.57)
     # seems it is 4.0+? https://github.com/bminor/bash/blob/f3b6bd19457e260b65d11f2712ec3da56cef463f/CHANGES#L5262-L5263
     set bash (brew --prefix bash)
