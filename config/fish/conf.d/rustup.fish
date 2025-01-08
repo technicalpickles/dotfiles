4b79b41506e81ff30bb7cb4073b47b482069ae00
Josh Nichols <josh.nichols@gusto.com>
2025-01-08 09:28:15 -0500

    Cleanup fish. remove version managers not using. fix dotpickles role detection, and use it in more places to prefer work setup for some things

--- config/fish/conf.d/rustup.fish
+++ config/fish/conf.d/rustup.fish
@@ -1,3 +1,3 @@
-if test -e "$HOME/.cargo/env.fish"
-  . "$HOME/.cargo/env.fish"
+if dotpickles_role "home" && test -e "$HOME/.cargo/env.fish"
+    . "$HOME/.cargo/env.fish"
 end
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,53 +1,37 @@
-if string match -q (hostname) josh-nichols
-    set DOTPICKLES_ROLE work
+if string match --quiet --regex '^josh-nichols-' (hostname)
+    set -gx DOTPICKLES_ROLE work
 else
-    set DOTPICKLES_ROLE home
+    set -gx DOTPICKLES_ROLE home
 end
 
-function fish_greeting
-    if type welcome2u >/dev/null 2>&1
-        welcome2u
-    end
-end
+if test -f  ~/.gusto/init.fish
+  source ~/.gusto/init.fish
+else
+  echo "using default init"
+  if test -z "$MISE_SHELL" && test -d "$HOME/.cargo/bin"
+      fish_add_path --global --prepend "$HOME/.cargo/bin"
+  end
 
-if [ (uname) = Darwin ]
+  if [ (uname) = Darwin ]
     # setup version manager
     if [ -x "$HOMEBREW_PREFIX/bin/mise" ]
-        # don't try to auto-install, so we things like the tide prompt don't trigger installations
-        set -gx MISE_NOT_FOUND_AUTO_INSTALL false
+      # don't try to auto-install, so we things like the tide prompt don't trigger installations
+      set -gx MISE_NOT_FOUND_AUTO_INSTALL false
 
-        # show the ruby installation happening, since it can take awhile
-        set -gx MISE_RUBY_VERBOSE_INSTALL true
+      # show the ruby installation happening, since it can take awhile
+      set -gx MISE_RUBY_VERBOSE_INSTALL true
 
-        mise activate --shims fish | source
-    else if test -d "$HOME/workspace/gdev-shell"
-        $HOME/workspace/gdev-shell/bin/gdev-shell init - fish | source
-        # if command -q pyenv
-        #   set -Ux PYENV_ROOT $HOME/.pyenv
-        #   set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-        #
-        #   pyenv init - | source
-        # end
-    else if [ "$DOTPICKLES_ROLE" = home ]
-        set asdf_path (brew --prefix asdf)/libexec/asdf.fish
-        if test -f $asdf_path
-            source $asdf_path
-        end
+      set -gx MISE_NODE_COREPACK true
+
+      mise activate --shims fish | source
     end
+  end
+end
 
+if [ -n "$HOMEBREW_PREFIX" ]
     if test -d "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
         fish_add_path --global --prepend --move "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
     end
-end
-
-# if running in nvim with unception, set the pipe for what neovim-remote will use
-if test -n "$NVIM_UNCEPTION_PIPE_PATH"
-    set -g NVIM_LISTEN_ADDRESS "$NVIM_UNCEPTION_PIPE_PATH"
-end
-
-
-if test -z "$MISE_SHELL" && test -d "$HOME/.cargo/bin"
-    fish_add_path --global --prepend "$HOME/.cargo/bin"
 end
 
 if status is-interactive
@@ -80,19 +64,6 @@ if status is-interactive
         alias pstree="pstree -g 2"
     end
 end
-
-# fix PATH to make sure ruby and node aren't using system or homebrew ruby
-if test -n "$RBENV_SHELL"
-    fish_add_path -g --prepend --move PATH "$HOME/.rbenv/shims"
-end
-
-if test -n "$NODENV_SHELL"
-    fish_add_path -g --prepend --move PATH "$HOME/.nodenv/shims"
-end
-
-# if test -n "$PYENV_ROOT"
-#   fish_add_path -g --prepend --move PATH "$PYENV_ROOT/shims"
-# end
 
 set -g --prepend PATH "$HOME/bin"
 
