8633d7cdebf4a7bb1a58e1848c03a509ff2ba3ae
Josh Nichols <josh.nichols@gusto.com>
2025-01-08 09:28:15 -0500

    Cleanup fish. remove version managers not using. fix dotpickles role detection, and use it in more places to prefer work setup for some things

--- config/fish/conf.d/git-duet.fish
+++ config/fish/conf.d/git-duet.fish
@@ -1,3 +1,5 @@
-set -gx GIT_DUET_CO_AUTHORED_BY true
-# need to use me first for commit signing
-set -gx GIT_DUET_ROTATE_AUTHOR false
+if dotpickles_role "home"
+  set -gx GIT_DUET_CO_AUTHORED_BY true
+  # need to use me first for commit signing
+  set -gx GIT_DUET_ROTATE_AUTHOR false
+end
