6cdf0cd4092a2e523625e7b18bf7e59f4f11503e
Josh Nichols <josh.nichols@gusto.com>
2025-10-21 07:46:47 -0400

    use env sense to detect running ide, instead of just checking availability of commands. default to nvim/vim/vi if not in an ide

--- config/fish/conf.d/editor.fish
+++ config/fish/conf.d/editor.fish
@@ -1,14 +1,27 @@
-# vscode is pretty alright when we're in it
-if string match -r -q insider "$TERM_PROGRAM_VERSION" && which code-insiders >/dev/null
-    set -gx EDITOR "code-insiders -w"
-else if [ "$TERM_PROGRAM" = vscode ] && which code >/dev/null
-    set -gx EDITOR "code -w"
-    # use stable while running inside stable
+# Use envsense to detect IDE context if available
+# See https://github.com/technicalpickles/envsense for detection logic
+if which envsense >/dev/null
+    # Get the IDE id once and use it multiple times
+    set -l ide_id (envsense info --json 2>/dev/null | jq -r '.traits.ide.id // empty')
+
+    # Check which IDE we're running in and set the appropriate editor
+    if test "$ide_id" = cursor && which cursor >/dev/null
+        set -gx EDITOR "cursor -w"
+    else if test "$ide_id" = vscode-insiders && which code-insiders >/dev/null
+        set -gx EDITOR "code-insiders -w"
+    else if test "$ide_id" = vscode && which code >/dev/null
+        set -gx EDITOR "code -w"
+    end
+end
+
+# Fallback to terminal editors if no IDE detected or IDE editor not found
 # we like vim. see https://github.com/technicalpickles/pickled-vim for settings
-else if which -s nvim >/dev/null
-    set -gx EDITOR nvim
-else if which -s vim >/dev/null
-    set -gx EDITOR vim
-else if which -s vi >/dev/null
-    set -gx EDITOR vi
+if not set -q EDITOR
+    if which -s nvim >/dev/null
+        set -gx EDITOR nvim
+    else if which -s vim >/dev/null
+        set -gx EDITOR vim
+    else if which -s vi >/dev/null
+        set -gx EDITOR vi
+    end
 end
